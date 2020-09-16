b40a77d851b0c24f411f11d5c390e48c4d9b7d8e
iDeveloperBrandon <hsy0602@outlook.com>
2020-09-16 15:51:24 +0900

    responsive header

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,10 @@
-console.log(123);
+const hamburger = document.getElementById("btnHamburger");
+const navbar = document.querySelector(".navbar");
+
+const about = document.querySelector(".about");
+const project = document.querySelector(".project");
+const contact = document.querySelector(".contact");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show-nav");
+});
