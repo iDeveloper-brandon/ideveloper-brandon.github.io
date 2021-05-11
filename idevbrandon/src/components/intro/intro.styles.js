65c256c07297dca02ac86b4f88e1c2bf9d1f2bb2
Brandon Ha <hsy0602@outlook.com>
2021-05-11 19:27:50 +0900

    style

--- idevbrandon/src/components/intro/intro.styles.js
+++ idevbrandon/src/components/intro/intro.styles.js
@@ -4,7 +4,7 @@ export const Intro = styled.div`
   padding-top: 25vh;
   height: 100vh;
   background-image: linear-gradient(
-    225deg,
+    50deg,
     rgb(24, 0, 200) 0%,
     rgb(185, 0, 180) 100%
   );
