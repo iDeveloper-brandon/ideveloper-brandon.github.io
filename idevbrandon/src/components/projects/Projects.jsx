53331ef658ea190c021189d174e8529e54181db3
Brandon Ha <hsy0602@outlook.com>
2021-05-11 19:38:14 +0900

    project

--- idevbrandon/src/components/projects/Projects.jsx
+++ idevbrandon/src/components/projects/Projects.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>top 3</div>;
+  return (
+    <div>
+      <h1>My big 3 projects</h1>
+      <ul>
+        <li>OXINION</li>
+        <li>Dividend King</li>
+        <li>RoboTaxi</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Projects;
